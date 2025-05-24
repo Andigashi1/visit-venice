540349b974252d3bfc235be3e06d68dc2ad30844
Andi Gashi <andig2413@gmail.com>
2025-05-24 23:26:17 +0200

    Added a burger menu for mobile view

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,25 +1,51 @@
-'use client'
+"use client";
 
-import logo from '@/public/logo.jpg'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { useState } from "react";
+import logo from "@/public/logo.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Menu, X } from "lucide-react";
 
 const Nav = () => {
-
   const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => setMenuOpen(!menuOpen)
 
   return (
-    <div className="z-100 fixed top-8 left-0 w-[98%] mx-4 py-2 px-10 bg-white border border-neutral-200 shadow-lg flex justify-between items-center rounded-xl">
-        <Image src={logo} className='w-20 h-12' alt='logo'/>
-        <div className="flex gap-6 font-bold [&>*]:cursor-pointer">
-            <Link href='/' className={pathname === '/' ? 'text-primary' : null}>Home</Link>
-            <Link href='/history' className={pathname === '/history' ? 'text-primary' : null}>History</Link>
-            <Link href='/places' className={pathname === '/places' ? 'text-primary' : null}>Places</Link>
-            <Link href='/gallery' className={`${pathname === '/gallery' ? 'text-primary' : null} cursor-pointer`}>Gallery</Link>
-        </div>
+    <div className="z-100 fixed top-8 left-4 right-4 py-2 px-5 md:px-10 bg-white border border-neutral-200 shadow-lg flex justify-between items-center rounded-xl">
+      <Image src={logo} className="w-20 h-12 max-md:hidden" alt="logo" />
+      <button onClick={toggleMenu} className="md:hidden">
+        {menuOpen ? <X size={28} /> : <Menu size={28} />}
+      </button>
+      <div className={`${menuOpen ? 'flex' : 'hidden'} md:flex gap-6 max-md:text-xs font-bold [&>*]:cursor-pointer`}>
+        <Link href="/" className={pathname === "/" ? "text-primary" : null}>
+          Home
+        </Link>
+        <Link
+          href="/history"
+          className={pathname === "/history" ? "text-primary" : null}
+        >
+          History
+        </Link>
+        <Link
+          href="/places"
+          className={pathname === "/places" ? "text-primary" : null}
+        >
+          Places
+        </Link>
+        <Link
+          href="/gallery"
+          className={`${
+            pathname === "/gallery" ? "text-primary" : null
+          } cursor-pointer`}
+        >
+          Gallery
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
